8a59ee4a74fec577c5397e2a2e68f5f01870d453
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-12-20 15:48:54 +0000

    fix: watch and build js setup

--- dev-utils/build-js.js
+++ dev-utils/build-js.js
@@ -1,8 +1,46 @@
 const rollup = require('rollup');
-const path = require('path')
+const path = require('path');
+const resolve = require("rollup-plugin-node-resolve");
+const replace = require("rollup-plugin-replace");
+const commonjs = require("rollup-plugin-commonjs");
+const svelte = require("rollup-plugin-svelte");
+const { terser } = require("rollup-plugin-terser");
+const json = require("rollup-plugin-json");
+const stimulus = require("rollup-plugin-stimulus")
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+const onwarn = (warning, onwarn) => onwarn(warning);
+const dedupe = importee =>
+  importee === "svelte" || importee.startsWith("svelte/");
 
 
-const inputOptions = [path.resolve(__dirname, "../src/components.js"), path.resolve(__dirname, "../src/stimulus.js")];
+const inputOptions = {
+  input: [path.resolve(__dirname, "../src/components.js"), path.resolve(__dirname, "../src/stimulus.js")],
+  plugins: [
+    json(),
+    replace({
+      "process.browser": true,
+      "process.env.NODE_ENV": JSON.stringify(mode)
+    }),
+    svelte({
+      dev,
+      emitCss: true
+    }),
+    stimulus(),
+    resolve({
+      browser: true,
+      dedupe
+    }),
+    commonjs(),
+
+    !dev &&
+      terser({
+        module: true
+      })
+  ],
+  onwarn};
 const outputOptions = {
   dir: path.resolve(__dirname, "../js"),
   format: "es",
--- dev-utils/watch-js.js
+++ dev-utils/watch-js.js
@@ -1,5 +1,20 @@
 const rollup = require('rollup');
 const path = require('path')
+const resolve = require("rollup-plugin-node-resolve");
+const replace = require("rollup-plugin-replace");
+const commonjs = require("rollup-plugin-commonjs");
+const svelte = require("rollup-plugin-svelte");
+const { terser } = require("rollup-plugin-terser");
+const json = require("rollup-plugin-json");
+const stimulus = require("rollup-plugin-stimulus")
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+const onwarn = (warning, onwarn) => onwarn(warning);
+const dedupe = importee =>
+  importee === "svelte" || importee.startsWith("svelte/");
+
 
 
 const inputOptions = [path.resolve(__dirname, "../src/components.js"), path.resolve(__dirname, "../src/stimulus.js")];
@@ -10,7 +25,32 @@ const outputOptions = {
 };
 
 function watch() {
-  const watcher = rollup.watch({input: inputOptions, output: outputOptions});
+  const watcher = rollup.watch({
+    input: inputOptions, 
+    plugins:  [
+      json(),
+      replace({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode)
+      }),
+      svelte({
+        dev,
+        emitCss: true
+      }),
+      stimulus(),
+      resolve({
+        browser: true,
+        dedupe
+      }),
+      commonjs(),
+  
+      !dev &&
+        terser({
+          module: true
+        })
+    ],
+    onwarn,
+    output: outputOptions});
   watcher.on('event', event => {
     switch (event.code) {
       case "START":
