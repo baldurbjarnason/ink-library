86db67105f1690e05ee5b4049905d54cd7ac74d7
Baldur Bjarnason <bb@baldurbjarnason.com>
2020-01-27 16:49:26 -0500

    Login first pass

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,137 @@
+
+import passport from "passport";
+import jwt from "jsonwebtoken";
+// import got from "got";
+import httpStrategies from "passport-http";
+import debugSetup from "debug";
+import Auth0Strategy from "passport-auth0";
+const debug = debugSetup("vonnegut:auth");
+const STRATEGY = process.env.PASSPORT_STRATEGY || 'basic';
+const SECRETORKEY = process.env.SECRETORKEY || 'this is a secret'
+const AUDIENCE = process.env.AUDIENCE || 'this is an audience'
+const ISSUER = process.env.ISSUER || 'this is an issuer'
+
+function generateToken(user) {
+  const expiresIn = "30m";
+  return jwt.sign({ sub: user.id }, SECRETORKEY, {
+    algorithm: "HS256",
+    expiresIn,
+    audience: AUDIENCE,
+    issuer: ISSUER
+  });
+}
+
+
+async function deserialise(user) {
+  if (!user.token) {
+    user.token = generateToken(user);
+  } else if (user.token) {
+    jwt.verify(
+      user.token,
+      SECRETORKEY,
+      {
+        algorithm: "HS256",
+        audience: AUDIENCE,
+        issuer: ISSUER
+      },
+      (err, decoded) => {
+        if (err && err.name === "TokenExpiredError") {
+          user.token = generateToken(user);
+        } else if (err) {
+          debug(err);
+          throw err;
+        }
+      }
+    );
+  }
+  // if (!user.profile || user.profile.status === 404) {
+  //   try {
+  //     const response = await got(`${process.env.API_SERVER}whoami`, {
+  //       headers: {
+  //         Authorization: `Bearer ${user.token}`
+  //       },
+  //       json: true
+  //     });
+  //     const { id, outbox } = response.body;
+  //     user.profile = { id, outbox };
+  //   } catch (err) {
+  //     user.profile = { status: err.statusCode };
+  //   }
+  // }
+  return user;
+}
+
+
 export function setup (app) {
+  passport.serializeUser((user, done) => {
+    // console.log('serialise: ', user)
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    // console.log('deserialise: ', user)
+    return deserialise(user)
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  if (process.env.PASSPORT_STRATEGY === "auth0") {
+    console.log('auth0')
+    passport.use(
+      new Auth0Strategy(
+        {
+          domain: process.env.AUTH0_DOMAIN,
+          clientID: process.env.AUTH0_CLIENT_ID,
+          clientSecret: process.env.AUTH0_CLIENT_SECRET,
+          callbackURL: process.env.CALLBACK_URL
+        },
+        (accessToken, refreshToken, extraParams, profile, done) => {
+          return done(null, { id: profile.id });
+        }
+      )
+    );
+    app.get("/callback", passport.authenticate("auth0", {}), function(
+      req,
+      res,
+      next
+    ) {
+      res.redirect(req.session.returnTo || "/");
+    });
+  } else {
+    console.log('basic')
+    passport.use(
+      new httpStrategies.BasicStrategy((username, password, callback) => {
+        console.log('using basic')
+        if (password === "devpassword") {
+          const user = { id: `dev-testing11|${username}` };
+          return callback(null, user);
+        } else {
+          return callback(null, false);
+        }
+      })
+    );
+  }
+
+  app.use(
+    "/login",
+    passport.authenticate(STRATEGY),
+    function(req, res, next) {
+      res.redirect(req.query.returnTo || "/");
+    }
+  );
+  app.use("/logout", (req, res) => {
+    if (!req.user) return res.redirect("/");
+    req.session.destroy(() => {
+      req.logout();
+      let redirect;
+      if (process.env.PASSPORT_STRATEGY === "auth0" && process.env.SIGNOUTURL) {
+        redirect = `https://${process.env.AUTH0_DOMAIN}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${process.env.SIGNOUTURL}`;
+      } else {
+        redirect = "/";
+      }
+      res.redirect(redirect);
+    });
+  });
   return app
 }
