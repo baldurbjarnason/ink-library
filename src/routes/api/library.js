08eed0fb760b833dcbdc950f6bf0a0906f56bae8
Baldur Bjarnason <bb@baldurbjarnason.com>
2020-01-30 13:19:36 -0500

    Workspace and stacks filter works

--- src/routes/api/library.js
+++ src/routes/api/library.js
@@ -1,8 +1,16 @@
 
 import got from "got";
 
+// This needs to filter by workspace
 export async function get(req, res, next) {
-  const response = await got(`${process.env.API_SERVER}library`, {
+  let url = `${process.env.API_SERVER}library`
+  if (req.query.stack) {
+    url = `${url}?stack=${req.query.stack}`
+  } else if (req.query.workspace) {
+    url = `${url}?stack=${req.query.workspace}`
+  }
+
+  const response = await got(url, {
     headers: {
       Authorization: `Bearer ${req.user.token}`
     }
