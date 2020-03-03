d73fbd0177964dbb6a5c2cfc3bb10a5412b173ed
Baldur Bjarnason <bb@baldurbjarnason.com>
2020-03-03 16:50:25 -0500

    Better error handling for API requests

--- src/stores/collections.js
+++ src/stores/collections.js
@@ -1,20 +1,20 @@
 
 import { derived, writable } from 'svelte/store';
+import {error} from './error.js'
+import {fetch} from './fetch.js'
 
 export const refreshCollections = writable(Date.now())
 
 export const tags = derived(refreshCollections, ($refreshCollections, set) => {
   set([])
   if (!process.browser) return
-  return window.fetch("/api/collections")
-    .then(res => {
-      return res.json()
-    })
+  return fetch("/api/collections")
     .then(lib => {
       set(lib)
     })
     .catch(err => {
       set({type: 'failed'})
+      error.set(err)
       console.error(err)
     })
 })
