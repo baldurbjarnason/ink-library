e6e42c807dd9faa929805b929197257efb96390b
Baldur Bjarnason <bb@baldurbjarnason.com>
2020-06-30 14:10:03 +0000

    Sign up code support

--- src/components/Auth/SignIn.svelte
+++ src/components/Auth/SignIn.svelte
@@ -10,11 +10,24 @@ export let path = '/'
     flex-direction: column;
     justify-content: center;
 }
+.SignIn form {
+  display: flex;
+  flex-direction: column;
+}
+.SignIn label {
+  margin: 1rem 0;
+}
 </style>
 
 <div class="SignIn">
 <h1>Sign In to Rebus Ink</h1>
-<form action="/login?returnTo={encodeURIComponent(path)}" method="POST"><button class="GenericButton">Login</button></form>
+<form action="/login?returnTo={encodeURIComponent(path)}" method="POST">
+
+<label>
+Beta Sign In Code <em>(required)</em><br>
+<input type="password" name="betaCode" required /></label>
+
+<button class="GenericButton">Login</button></form>
 <!-- 
 <p>Or, if you have never used Ink before:</p>
 <p>
