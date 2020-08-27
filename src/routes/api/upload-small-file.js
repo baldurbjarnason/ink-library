9fdb242c54fa08499c0a3c5569aee21b5331bf5d
Baldur Bjarnason <baldur@rebus.foundation>
2020-08-27 19:37:09 +0000

    Markdown processing for small files.

--- src/routes/api/upload-small-file.js
+++ src/routes/api/upload-small-file.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const os = require("os");
 const formats = require("ink-engine/src/formats");
 const compressible = require('compressible')
+const MarkdownIt = require('markdown-it')
+const md = new MarkdownIt();
 const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
 
@@ -12,18 +14,24 @@ export async function post(req, res, next) {
   const fileName = path.basename(req.query.filePath || 'index');
   let contentType
   let suffix
+  let body
   if (req.is("html")) {
     contentType = "text/html"
     suffix = "html"
+    body = req.body
   } else if (req.is('application/epub+zip')) {
     contentType = "application/epub+zip"
     suffix = "epub"
+    body = req.body
   } else if (req.is("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
     contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     suffix = "docx"
+    body = req.body
   } else if (req.is("text/*")) {
     contentType = "text/markdown"
     suffix = "md"
+    const text = req.body.toString()
+    body = wrap(md.render(text), fileName)
   }
   const {storageId, url, userPrefix} = await generateURL(`${fileName}.${suffix}`, req.user.profile.id)
   const filePrefix = storageId;
@@ -31,7 +39,7 @@ export async function post(req, res, next) {
   const tempFilePath = path.join(os.tmpdir(), userPrefix, filePrefix, fileName);
   const targetPath = path.join(userPrefix, filePrefix);
   await fs.promises.mkdir(path.join(os.tmpdir(), userPrefix, filePrefix), {recursive: true})
-  await fs.promises.writeFile(tempFilePath, req.body)
+  await fs.promises.writeFile(tempFilePath, body)
   const processor = new Format(tempFilePath, { extract, type: contentType })
   const book = await processor.process();
   fs.unlinkSync(tempFilePath);
@@ -54,4 +62,19 @@ export async function post(req, res, next) {
     });
     return filename;
   }
+}
+
+function wrap (body, title) {
+  return `<!doctype html>
+  <html>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width,initial-scale=1.0'>
+    <title>${title}</title>
+  </head>
+  <body id="body">
+  ${body}
+  </body>
+  </html>
+  `;
 }
