aed64cc3d0d9b43143f18b333454883f57e0f5f8
Baldur Bjarnason <baldur@rebus.foundation>
2020-10-08 12:47:16 +0000

    Testing actions

--- save-config.js
+++ save-config.js
@@ -2,4 +2,7 @@ const fs = require("fs")
 
 if (process.env.APP_ENGINE_YAML) {
   fs.writeFileSync("app.yaml", process.env.APP_ENGINE_YAML)
+  console.log("saved a secret")
+} else {
+  console.log("did not save a secret")
 }
