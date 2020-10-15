5e4dd41d06c82d700c12fdd152ee51a14c3d2522
Baldur Bjarnason <baldur@rebus.foundation>
2020-10-15 16:55:20 +0000

    Figure out the auth error for dev

--- src/routes/api/publication/placeholder.js
+++ src/routes/api/publication/placeholder.js
@@ -1,5 +1,5 @@
 export async function get(req, res, next) {
-  if (!req.user.profile) return res.sendStatus(401);
+  if (!req.user || !req.user.profile) return res.sendStatus(401);
   res.set("Cache-Control", "max-age=31536000, immutable");
   res.json({ type: "loading", items: [], tags: [], keywords: [], replies: [] });
 }
