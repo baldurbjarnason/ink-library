cf0ab04b65da8b7dd4721fa1e4a73f2f6c77e341
Baldur Bjarnason <baldur@rebus.foundation>
2021-01-18 22:35:51 +0000

    Test config fix

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -10,7 +10,7 @@ module.exports = {
   coverage: true,
   coverageConfig: {
     report: true,
-    exclude: ["node_modules/**/*"]
+    exclude: ["node_modules/**/*", "test/utils.js"]
   },
   browsers: [
     chromeLauncher({
