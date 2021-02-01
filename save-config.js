2a085b97f182d5a67fd29cc197a3e30f23cf210c
Baldur Bjarnason <baldur@rebus.foundation>
2021-02-01 20:45:17 +0000

    Config saving

--- save-config.js
+++ save-config.js
@@ -6,3 +6,9 @@ if (process.env.APP_ENGINE_YAML) {
 } else {
   console.log("did not save a secret")
 }
+if (process.env.GOOGLE_KEYFILE) {
+  fs.writeFileSync("keyfile.json", process.env.GOOGLE_KEYFILE)
+  console.log("saved a secret")
+} else {
+  console.log("did not save a secret")
+}
