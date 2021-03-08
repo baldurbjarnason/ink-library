ce675289fafd8dd1a5a7ca467977d56dde051ddf
Baldur Bjarnason <baldur@rebus.foundation>
2021-03-08 14:32:43 +0000

    Better 404 errors for deleted sources

--- src/routes/sources/[id]/index.svelte
+++ src/routes/sources/[id]/index.svelte
@@ -1,11 +1,13 @@
 <script context="module">
-	export async function preload({params, query, path}) {
+  export async function preload({ params, query, path }) {
     const res = await this.fetch(`/api/sources/${params.id}`);
-    const source = await res.json()
-    if (source.json.storageId) {
-      return this.redirect(302, `${path}/${source.json.storageId}`)
+    const source = await res.json();
+    if (!source || source.statusCode === 404) {
+      return this.error(404, "This source doesn't exist or has been deleted");
+    } else if (source.json.storageId) {
+      return this.redirect(302, `${path}/${source.json.storageId}`);
     } else {
-      return this.redirect(302, `${path}/empty`)
+      return this.redirect(302, `${path}/empty`);
     }
   }
 </script>
--- src/routes/target/[id]/[...path].svelte
+++ src/routes/target/[id]/[...path].svelte
@@ -1,11 +1,18 @@
 <script context="module">
-	export async function preload({params, query, path}) {
+  export async function preload({ params, query, path }) {
     const res = await this.fetch(`/api/sources/${params.id}`);
-    const source = await res.json()
-    if (source.json.storageId) {
-      return this.redirect(302, `/sources/${params.id}/${source.json.storageId}/${params.path.join("/")}`)
+    const source = await res.json();
+    if (!source || source.statusCode === 404) {
+      return this.error(404, "This source doesn't exist or has been deleted");
+    } else if (source.json.storageId) {
+      return this.redirect(
+        302,
+        `/sources/${params.id}/${source.json.storageId}/${params.path.join(
+          "/"
+        )}`
+      );
     } else {
-      return this.redirect(302, `${path}/empty`)
+      return this.redirect(302, `${path}/empty`);
     }
   }
 </script>
