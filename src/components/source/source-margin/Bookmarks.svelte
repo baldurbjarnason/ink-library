d36c0e2f2c251098a537f9beb94aad2e9933a846
Baldur Bjarnason <baldur@rebus.foundation>
2021-03-10 13:30:04 +0000

    Make sure bookmarks appear

--- src/components/source/source-margin/Bookmarks.svelte
+++ src/components/source/source-margin/Bookmarks.svelte
@@ -1,23 +1,17 @@
 <script>
   import { Bookmark } from "../../../../state/models/Bookmark";
-  import { intersections, topmost } from "../../../../state/nodes";
+  import { intersections } from "../../../../state/nodes";
   import BookmarkButton from "./BookmarkButton.svelte";
   export let root;
   export let bookmarks;
   const headings$ = intersections(
-    "h1:not([data-ink-page-title]):not([data-ink-private]),h2:not(#navigation-header):not(#toc-heading):not([data-ink-private]),h3,h4,h5"
+    "h1:not([data-ink-page-title]):not([data-ink-private]),h2:not(#navigation-header):not(#toc-heading):not([data-ink-private]),h3,h4"
   );
-  const topmostBookmarked$ = topmost(headings$, (entry) => {
-    return entry.element.dataset.annotationId;
-  });
-  const topmostNotBookmarked$ = topmost(headings$, (entry) => {
-    return !entry.element.dataset.annotationId;
-  });
   function findMark(entry) {
     const mark = bookmarks.find((mark) => {
       if (mark.id === entry.element.dataset.annotationId) return mark;
     });
-    return mark;
+    return mark || new Bookmark({});
   }
 </script>
 
@@ -29,19 +23,19 @@
 
 <div class="Root">
   {#if root}
-    {#if $topmostBookmarked$}
-      {#each Array.from($topmostBookmarked$) as entry}
+    {#if $headings$}
+      {#each Array.from($headings$) as entry}
         {#if findMark(entry)}
           <BookmarkButton {entry} bookmark={findMark(entry)} />
         {/if}
       {/each}
     {/if}
-    {#if $topmostNotBookmarked$}
+    <!-- {#if $topmostNotBookmarked$}
       {#each Array.from($topmostNotBookmarked$) as entry}
         {#if entry.element.offsetParent !== null}
           <BookmarkButton {entry} bookmark={new Bookmark({})} />
         {/if}
       {/each}
-    {/if}
+    {/if} -->
   {/if}
 </div>
