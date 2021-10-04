bd16a22aa1fa6ece60dec1eb5c11adc249f93225
Marie Pelletier <marie.pelletier@gmail.com>
2021-10-04 14:32:08 -0400

    test

--- src/components/pages/Tools/OutlineTools.svelte
+++ src/components/pages/Tools/OutlineTools.svelte
@@ -19,8 +19,10 @@
   export let addNewNote;
   export let filters;
   export let disabled;
+
+  console.log(process.env.NODE_ENV)
   // NOTE FROM MARIE: don't know why the api-server is wrong on production, but hard coding seems to fix it for now.
-  let server = process.env.NODE_ENV==="development" ? process.env.API_SERVER : "https://ink-api-dev-dot-thematic-cider-139815.appspot.com/" 
+  let server = process.env.NODE_ENV==="development" || "dev" ? process.env.API_SERVER : "https://ink-api-dev-dot-thematic-cider-139815.appspot.com/" 
 
   let checkAll = () => {
     filters.type = [];
